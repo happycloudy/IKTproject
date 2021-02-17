186d86c0c8bd86f556a7728c5fc9b8351defba4d
Yury <altnightfall@mail.ru>
2021-02-17 16:48:04 +0300

    Matrix

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Header from '../components/header'
 import Banner from '../components/banner'
 import Footer from '../components/footer'
+import News from '../components/news'
 
 export default function Home() {
   return (
