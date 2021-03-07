7f9eab7a6fc5ee739ff7f98b12e4b834e16c4f07
Yury <altnightfall@mail.ru>
2021-03-08 00:18:16 +0300

    deleted comments

--- components/news.js
+++ components/news.js
@@ -14,53 +14,6 @@ const containerStyle ={
 }
 
 export default class News extends React.Component {
-    /*constructor(props){
-        super(props)
-        this.getMatrix = this.getMatrix.bind(this); 
-    }
-    componentDidMount(){
-        this.getMatrix();
-    }
-
-    getMatrix = async (e) => {
-        let c = document.getElementById("c");
-        let ctx = c.getContext("2d");
-
-        c.height = window.innerHeight /2;
-        c.width = window.innerWidth;
-
-        let matrix = "fuck";
-
-        matrix = matrix.split("");
-
-        let font_size = 10;
-        let columns = c.width/font_size; 
-        let drops = [];
-        
-        for(let x = 0; x < columns; x++)
-            drops[x] = 1; 
-
-
-        function draw()
-        {
-            ctx.fillStyle = "rgba(0, 0, 20, 0.04)";
-            ctx.fillRect(0, 0, c.width, c.height);
-
-            ctx.fillStyle = "#308DE4";
-            ctx.font = font_size + "px arial";
-            for(let i = 0; i < drops.length; i++)
-            {
-                let text = matrix[Math.floor(Math.random()*matrix.length)];
-                ctx.fillText(text, i*font_size, drops[i]*font_size);
-                if(drops[i]*font_size > c.height && Math.random() > 0.975)
-                    drops[i] = 0;
-                drops[i]++;
-            }
-        }
-        setInterval(draw, 35);
-    }
-    <canvas id="c" style={{position:'absolute',background:'black',width:'100%',height:'100%'}}/>*/
-
 render(){
     return(
     <div style={{position: 'relative'}}>
