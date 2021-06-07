8ccd348bb5df10c498deba3505d4ddfdf66381da
Altnightfall <Altnightfall@yandex.ru>
2021-06-07 13:28:11 +0300

    Fixed navbar colors

--- components/general/header.js
+++ components/general/header.js
@@ -1,4 +1,4 @@
-import {Navbar, Nav, NavDropdown} from 'react-bootstrap'
+import {Navbar, Nav, NavDropdown, Dropdown} from 'react-bootstrap'
 
 
 export default function Header(props) {
@@ -7,10 +7,7 @@ export default function Header(props) {
         background: 'linear-gradient(right, rgba(15,76,134,1), #308DE4 100%)', /* W3C */
         background: '-webkit-linear-gradient(right, rgba(15,76,134,0.8), #308DE4 100%)',
     }
-    const DropdownStyle = {
-        color : 'white !important',
-        textDecoration: 'none',
-    }
+
     return (
         <div>
             <Navbar
@@ -30,19 +27,23 @@ export default function Header(props) {
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
                     <Nav.Link className="text-white" href="/">Главная</Nav.Link>
-                    <NavDropdown  style={DropdownStyle} title="Студентам" id="collasible-nav-dropdown">
-                        <NavDropdown.Item href="/infoForStudents">Студентам</NavDropdown.Item>
-                        <NavDropdown.Item href="/grants">О&nbsp;грантах</NavDropdown.Item>
-                        <NavDropdown.Item href="/olympic">Олимпиады</NavDropdown.Item>
-                        <NavDropdown.Item href="/conferencesAndStateGrants">Конференции&nbsp;и<br/>гос.&nbsp;гранты</NavDropdown.Item>
-                        <NavDropdown.Item href="/publicationsAndStudentsWorks">Публикации&nbsp;и<br/>студенческие&nbsp;работы</NavDropdown.Item>
+                    <NavDropdown title={
+                        <span className="text-white my-auto">Студентам</span>
+                    } id="collasible-nav-dropdown">
+                        <NavDropdown.Item style={{color:"#308DE4"}} href="/infoForStudents">Студентам</NavDropdown.Item>
+                        <NavDropdown.Item style={{color:"#308DE4"}} href="/grants">О&nbsp;грантах</NavDropdown.Item>
+                        <NavDropdown.Item style={{color:"#308DE4"}} href="/olympic">Олимпиады</NavDropdown.Item>
+                        <NavDropdown.Item style={{color:"#308DE4"}} href="/conferencesAndStateGrants">Конференции&nbsp;и<br/>гос.&nbsp;гранты</NavDropdown.Item>
+                        <NavDropdown.Item style={{color:"#308DE4"}} href="/publicationsAndStudentsWorks">Публикации&nbsp;и<br/>студенческие&nbsp;работы</NavDropdown.Item>
                     </NavDropdown>
-                    <NavDropdown style={DropdownStyle} title="Аспирантам" id="collasible-nav-dropdown">
-                        <NavDropdown.Item href="/graduate">Аспирантам</NavDropdown.Item>
-                        <NavDropdown.Item href="/grants">О&nbsp;грантах</NavDropdown.Item>
-                        <NavDropdown.Item href="/olympic">Олимпиады</NavDropdown.Item>
-                        <NavDropdown.Item href="/conferencesAndStateGrants">Конференции&nbsp;и<br/>гос.&nbsp;гранты</NavDropdown.Item>
-                        <NavDropdown.Item href="/publicationsAndStudentsWorks">Публикации&nbsp;и<br/>студенческие&nbsp;работы</NavDropdown.Item>
+                        <NavDropdown title={
+                            <span className="text-white my-auto">Аспирантам</span>
+                        } id="collasible-nav-dropdown">
+                        <NavDropdown.Item style={{color:"#308DE4"}} href="/graduate">Аспирантам</NavDropdown.Item>
+                        <NavDropdown.Item style={{color:"#308DE4"}} href="/grants">О&nbsp;грантах</NavDropdown.Item>
+                        <NavDropdown.Item style={{color:"#308DE4"}} href="/olympic">Олимпиады</NavDropdown.Item>
+                        <NavDropdown.Item style={{color:"#308DE4"}} href="/conferencesAndStateGrants">Конференции&nbsp;и<br/>гос.&nbsp;гранты</NavDropdown.Item>
+                        <NavDropdown.Item  style={{color:"#308DE4"}} href="/publicationsAndStudentsWorks">Публикации&nbsp;и<br/>студенческие&nbsp;работы</NavDropdown.Item>
                     </NavDropdown>
                     <Nav.Link className="text-white" href="/infoForEntrants">Абитуриентам</Nav.Link>
                     <Nav.Link className="text-white" href="/teachers">Преподаватели</Nav.Link>
